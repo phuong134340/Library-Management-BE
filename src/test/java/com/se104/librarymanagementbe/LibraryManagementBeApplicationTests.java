7d7928f7c566766789d77471b13de13e332863a3
phuong_22521167 <22521167@gm.uit.edu.vn>
2024-03-22 23:13:27 +0700

    first commit

---
+++ src/test/java/com/se104/librarymanagementbe/LibraryManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se104.librarymanagementbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
