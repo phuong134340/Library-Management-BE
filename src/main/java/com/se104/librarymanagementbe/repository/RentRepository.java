82acf56167b9b132f0ad45e5d60f98e3d6a49b20
phuong_22521167 <22521167@gm.uit.edu.vn>
2024-05-23 23:15:08 +0700

    update RentService

--- src/main/java/com/se104/librarymanagementbe/repository/RentRepository.java
+++ src/main/java/com/se104/librarymanagementbe/repository/RentRepository.java
@@ -4,6 +4,9 @@ import com.se104.librarymanagementbe.entity.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RentRepository extends JpaRepository<Rent, Long> {
+    List<Rent> findAllByReaderIdAndStatusIs(long readerId, String status);
 }
--- src/main/java/com/se104/librarymanagementbe/service/RentService.java
+++ src/main/java/com/se104/librarymanagementbe/service/RentService.java
@@ -15,9 +15,11 @@ import com.se104.librarymanagementbe.repository.ReaderRepository;
 import com.se104.librarymanagementbe.repository.RentRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.text.CollationElementIterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -25,6 +27,7 @@ import java.util.stream.Collectors;
 @Service
 @AllArgsConstructor
 public class RentService {
+    @Autowired
     private final RentRepository rentRepository;
     private final ModelMapper mapper;
     private final BookRepository bookRepository;
@@ -50,16 +53,51 @@ public class RentService {
             return null;
         }
         Optional<Reader> reader = readerRepository.findById(rent.getReaderId());
+        if(reader.isEmpty()){
+            return null;
+        }
+//        List<Rent> totalRent = rentRepository.findAllByReaderIdAndStatusIs(rent.getReaderId(), rent.getStatus());
+//        if(totalRent > ConfigLibrary.getLimitBook() ){
+//            return null;
+//        }
+        Rent newRent = mapper.map(rent, Rent.class);
+        newRent.setBook(book.get());
+        newRent.setReader(reader.get());
+        rentRepository.save(newRent);
         return RestResponse.<CreateRentResponse>builder()
                 .status(HttpStatus.CREATED.value())
-                .data(mapper.map(res, CreateRentResponse.class))
+                .data(mapper.map(newRent, CreateRentResponse.class))
                 .build();
     }
 
     public RestResponse<UpdateRentResponse> updateRent(UpdateRentRequest rent, Long id) {
         Optional<Rent> oldRent = rentRepository.findById(id);
         if (oldRent.isPresent()) {
-
+            if(rent.getBookId() != 0){
+                Optional<Book> book = bookRepository.findById(rent.getBookId());
+                if(book.isEmpty()){
+                    return null;
+                }else{
+                    oldRent.get().setBook(book.get());
+                }
+            }
+            if(rent.getReaderId() != 0){
+                Optional<Reader> reader = readerRepository.findById(rent.getReaderId());
+                if(reader.isEmpty()){
+                    return null;
+                }else{
+                    oldRent.get().setReader(reader.get());
+                }
+            }
+            if(rent.getStartDate() != null){
+                oldRent.get().setStartDate(rent.getStartDate());
+            }
+            if(rent.getEndDate() != null){
+                oldRent.get().setEndDate(rent.getStartDate());
+            }
+            if(rent.getStatus() != null){
+                oldRent.get().setStatus(rent.getStatus());
+            }
             rentRepository.save(oldRent.get());
             return RestResponse.<UpdateRentResponse>builder()
                     .status(HttpStatus.OK.value())
